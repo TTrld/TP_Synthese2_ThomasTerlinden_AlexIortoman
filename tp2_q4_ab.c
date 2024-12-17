ace1333bfd331c8b38afe9a94adb664b4cc0bc6d
A1eX120 <alex.iortoman@ensea.fr>
2024-12-17 10:56:13 +0100

    Add files via upload

--- tp2_q4_ab.c
+++ tp2_q4_ab.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 #define BUF_SIZE 32
+#define BUF_ACKSIZE 4
 
 int main(int argc, char *argv[]){
 	
@@ -18,11 +19,13 @@ int main(int argc, char *argv[]){
 	
 	int clientSocket; 
 	ssize_t sndto; 
+	ssize_t rcvFrom;
 	struct addrinfo hints; 
 	struct addrinfo *results; 
 	struct addrinfo *record;
 	
 	char buf_senddata[BUF_SIZE] = {};
+	char buf_ackdata[BUF_ACKSIZE] = {};
 		
 	memset(&hints, 0, sizeof(struct addrinfo));
 	
@@ -48,7 +51,7 @@ int main(int argc, char *argv[]){
 	--------------------------------------------------
 	*/	
 	char *mode = "octet";
-	int nbchar = 0;
+	size_t nbchar = 0;
 	buf_senddata[0] = 0; // Opcode 1/2
 	buf_senddata[1] = 1; // Opcode 2/2 : Read request (RRQ)
 	nbchar = nbchar + 2;
@@ -70,18 +73,39 @@ int main(int argc, char *argv[]){
 		senddata[i] = buf_senddata[i];
 		printf("senddata[%d] = %c\n", i, senddata[i]);
 	}
-	printf("nchar = %d\n", nbchar);
-		
-	//char *sendDataTab = (char *)malloc((3 + strlen(fileName) + strlen(mode)) * sizeof(char));
-	
+	//printf("nchar = %d\n", nbchar);
 	
 	sndto = sendto(clientSocket, senddata, nbchar, MSG_EOR, results->ai_addr, results->ai_addrlen);
 	if (sndto < 0){
 		perror("Failed to send data.");
 		exit(EXIT_FAILURE);
 	}
-
-	//free(sendDataTab);
+		
+	ssize_t nbackpck = 0;
+	buf_ackdata[0] = 0;
+	buf_ackdata[1] = 4;
+	buf_ackdata[2] = 0;
+	buf_ackdata[3] = 1;	
+	nbackpck = nbackpck + 4;
+	
+	struct sockaddr * ptACKServerPort;
+	struct sockaddr_in ServerResponse;
+		
+	rcvFrom = recvfrom(clientSocket, senddata, nbchar, 0, ptACKServerPort, &(results->ai_addrlen));
+	if (rcvFrom < 0){
+		perror("Receive data failed.");
+		exit(EXIT_FAILURE);
+	}
+	
+	//printf("ACKServerPort : %d", ptACKServerPort->sa_family);
+	
+	sndto = sendto(clientSocket, buf_ackdata, nbackpck, 0, ptACKServerPort, (results->ai_addrlen));
+	if (sndto < 0){
+		perror("ACK failed.");
+		exit(EXIT_FAILURE);
+	}
+	
+	
 	printf("Client ended.\n");
 	
 }
